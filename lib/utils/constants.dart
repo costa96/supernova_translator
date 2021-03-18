74deed52bdcbff2f8c4f0dafedc782d31896888b
Gabriel <cn.gabri3l@gmail.com>
2021-03-18 19:05:50 +0100

    Created models files

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+const String API_KEY = "AIzaSyDFXUXM2C7dY-FXJG9xKkzYD5e5Dhe5Vs";
+const String API_ENDPOINT =
+    "https://translation.googleapis.com/language/translate/v2/languages";
+
+// https://translation.googleapis.com/language/translate/v2/languages?key=AIzaSyDFXUXM2C7dY-FXJG9xKkzYD5e5Dhe5Vs0&target=en
