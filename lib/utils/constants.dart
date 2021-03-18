b0a3ceac6be7c3f15e36d0edf855218ee0fd72a1
Gabriel <cn.gabri3l@gmail.com>
2021-03-18 19:34:53 +0100

    Updated constants to manage google's translation API

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,10 @@
-const String API_KEY = "AIzaSyDFXUXM2C7dY-FXJG9xKkzYD5e5Dhe5Vs";
-const String API_ENDPOINT =
-    "https://translation.googleapis.com/language/translate/v2/languages";
+const String GOOGLE_API_KEY = "AIzaSyDFXUXM2C7dY-FXJG9xKkzYD5e5Dhe5Vs";
+
+const String GOOGLE_TRANSLATE_API_BASE_URL =
+    "https://translation.googleapis.com";
+
+const String API_TRANSLATION_SERVICE = "/language/translate/v2";
+
+const String LANGUAGES_ENDPOINT = "/languages";
 
 // https://translation.googleapis.com/language/translate/v2/languages?key=AIzaSyDFXUXM2C7dY-FXJG9xKkzYD5e5Dhe5Vs0&target=en
